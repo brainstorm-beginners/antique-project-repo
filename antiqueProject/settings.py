094a3cc166deb3f282659ba259f986a114f5543c
Igor Ruzhilov <iruzhilov@gmail.com>
2024-10-13 13:34:54 +0500

    Add 'MEDIA_URL' parameter to the 'settings.py'.

--- antiqueProject/settings.py
+++ antiqueProject/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from django.template.context_processors import media
 
 from antiqueProject.config import project_config
 
@@ -128,3 +130,6 @@ STATIC_URL = 'C:/Users/User/PycharmProjects/antiqueProject/static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
