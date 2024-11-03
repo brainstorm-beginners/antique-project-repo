194d5268dac8f07dfa091bb50f8577e91d09c604
shell679 <nyancat2452@gmail.com>
2024-11-03 15:16:39 +0500

    Add products page and fix bug with mobile menu

--- static/contactPage.js
+++ static/contactPage.js
@@ -1,3 +1,84 @@
+function setSelectedCategory(categoryName) {
+    localStorage.setItem("selectedCategory", categoryName);
+  }
+  
+  function getSelectedCategory() {
+    return localStorage.getItem("selectedCategory");
+  }
+  
+  // Функция выделения выбранной категории
+  function highlightSelectedCategory() {
+    const selectedCategory = getSelectedCategory();
+    const categories = document.querySelectorAll(".category, .subcategory, .category__dropdown");
+    categories.forEach((category) => {
+      if (category.textContent.trim() === selectedCategory) {
+        category.classList.add("selected");
+      } else {
+        category.classList.remove("selected");
+      }
+    });
+  }
+  
+  window.addEventListener("DOMContentLoaded", () => {
+    // Настройка мобильного меню
+    const mobileMenuButton = document.getElementById('mobileMenuButton');
+    const mobileMenu = document.querySelector('.mobileMenu');
+    const mobileMenuButtonIcon = document.querySelector('.mobileMenuButtonIcon');
+    const menuIcon = '../static/icons/menuIconLightGrey.png';
+    const closeIcon = '../static/icons/closeMobileMenuIcon.png';
+  
+    mobileMenuButton.addEventListener('click', () => {
+      mobileMenu.style.display = mobileMenu.style.display === 'none' ? 'block' : 'none';
+  
+      mobileMenuButtonIcon.src = mobileMenuButtonIcon.src.includes('menuIconLightGrey.png') ? closeIcon : menuIcon;
+    });
+  
+    const dropdownCategories = document.querySelectorAll(".category__dropdown");
+    const categories = document.querySelectorAll(".category, .subcategory, .category__dropdown");
+  
+    // Открытие и закрытие подкатегорий при клике
+    dropdownCategories.forEach((category) => {
+      category.addEventListener("click", function (event) {
+        event.stopPropagation(); // Останавливаем всплытие события
+        const subcategoriesMenu = category.querySelector(".subcategoriesMenu");
+  
+        if (subcategoriesMenu.style.display === "block") {
+          subcategoriesMenu.style.display = "none";
+          category.classList.remove("open");
+        } else {
+          document.querySelectorAll(".subcategoriesMenu").forEach((menu) => {
+            menu.style.display = "none";
+            menu.parentElement.classList.remove("open");
+          });
+          
+          subcategoriesMenu.style.display = "block";
+          category.classList.add("open");
+        }
+      });
+    });
+  
+    // Клик на обычную категорию или подкатегорию для перехода по URL и выделения
+    categories.forEach((category) => {
+      category.addEventListener("click", function () {
+        // Проверяем, имеет ли категория подкатегории
+        if (!category.classList.contains("category__dropdown")) {
+          const categoryName = category.textContent.trim();
+  
+          // Сохраняем выбранную категорию
+          setSelectedCategory(categoryName);
+  
+          // Переход на URL с категорией
+          const urlCategoryName = categoryName.toLowerCase().replace(" ", "_");
+          window.location.href = `/api/v1/${urlCategoryName}`;
+        }
+      });
+    });
+  
+    // Подсвечиваем выбранную категорию при загрузке страницы
+    highlightSelectedCategory();
+  });
+  
+
 document.addEventListener('DOMContentLoaded', async () => {
 
 
@@ -10,8 +91,8 @@ document.addEventListener('DOMContentLoaded', async () => {
             document.getElementById('map'), 
             {
                 location: {
-                    center: [76.982018, 43.334709],
-                    zoom: 14                            
+                    center: [76.982152, 43.335142],
+                    zoom: 17,                         
                 }
             }
         );
@@ -23,22 +104,24 @@ document.addEventListener('DOMContentLoaded', async () => {
         markerElement.className = 'marker-class';
 
         const image = document.createElement('img');
-        image.src = '/static/icons/map-marker-alt.svg'; // Замените на URL вашего изображения
-        image.alt = 'Marker Icon'; // Альтернативный текст для изображения
-        image.style.width = '64px'; // Устанавливаем ширину изображения
-        image.style.height = '64px';
+        image.src = '/static/icons/map-marker-alt.svg';
+        image.alt = 'Marker Icon';
+        image.style.width = '48px'; 
+        image.style.height = '48px';
+        image.style.transform = 'translate(-50%, -100%)'
 
         markerElement.appendChild(image);
 
         const marker = new YMapMarker(
-        {
-            coordinates: [76.982018, 43.334709],
-            draggable: true,
+        {   
+            coordinates: [76.982152, 43.335142],
+            draggable: false,
         },
         markerElement
-        );
+    );
 
         map.addChild(marker);
+    
     }
 
     await initMap();
