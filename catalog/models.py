73d0f963af251d0a51743527b506fbb05cbce98b
Igor Ruzhilov <iruzhilov@gmail.com>
2024-11-03 18:44:08 +0500

    Development: Set default last 10 products added to the main page.

--- catalog/models.py
+++ catalog/models.py
@@ -7,6 +7,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена товара")
     slug = models.SlugField(unique=True, verbose_name="URL товара")
 
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return f"Товар: {self.name}(Описание: {self.description}, Категория: {self.category}, Цена: {self.price}, URL товара: {self.slug})"
 
@@ -18,6 +20,8 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='images', verbose_name="Товар")
     image = models.ImageField(upload_to='products/', verbose_name="Изображение товара")
+
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Фото для {self.product.name}"
@@ -34,6 +38,8 @@ class Category(models.Model):
         verbose_name="Родительская категория"
     )
     slug = models.SlugField(unique=True, verbose_name="URL категории")
+
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
--- catalog/views.py
+++ catalog/views.py
@@ -5,8 +5,9 @@ from .models import Product, Category
 
 def main_page(request: WSGIRequest) -> render:
     categories = Category.objects.filter(parent__isnull=True).prefetch_related('subcategories')
+    last_ten_products = Product.objects.order_by('-created_at')[:10]
 
-    return render(request, 'mainPage.html', {'root_categories': categories})
+    return render(request, 'mainPage.html', {'root_categories': categories, 'products': last_ten_products})
 
 def product_page(request: WSGIRequest, product_slug: str) -> render:
     product_url = get_object_or_404(Product, slug=product_slug)
@@ -18,4 +19,4 @@ def products_page(request: WSGIRequest) -> render:
     categories = Category.objects.filter(parent__isnull=True).prefetch_related('subcategories')
     products = Product.objects.all()
 
-    return render(request, 'productsPage.html', {'product': products, 'root_categories': categories})
+    return render(request, 'productsPage.html', {'products': products, 'root_categories': categories})
